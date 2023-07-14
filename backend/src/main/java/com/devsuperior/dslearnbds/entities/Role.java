0e4c17eba5ddc5bac9350f9b36ad2eb618f411c5
EduardoGomes1982 <edu1982talk@gmail.com>
2023-07-13 22:15:10 -0300

    Jpa Entity start

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -1,5 +1,11 @@
 package com.devsuperior.dslearnbds.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -8,8 +14,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
 public class Role {
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String authority;
