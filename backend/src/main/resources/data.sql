e001b08074338fae270b1473a9a307a1c0c730db
EduardoGomes1982 <edu1982talk@gmail.com>
2023-07-13 23:09:09 -0300

    Jpa Entity Course start

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role(user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role(user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role(user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role(user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://cursos-gratuitos-sp.com/wp-content/uploads/2020/03/cursos-gratuitos-duque-de-caxias.jpg', 'https://coerveracademy.es/wp-content/uploads/2016/11/Foto1.png');
