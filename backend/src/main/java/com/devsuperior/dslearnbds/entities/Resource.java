3fd45b024e4aab04c6f70ced069f26db590bb89a
EduardoGomes1982 <edu1982talk@gmail.com>
2023-07-15 21:51:21 -0300

    Start Resource Entity and Offer mapped

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -1,17 +1,14 @@
 package com.devsuperior.dslearnbds.entities;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.enums.ResourceType;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -23,8 +20,8 @@ import lombok.RequiredArgsConstructor;
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Entity
-@Table(name = "tb_offer")
-public class Offer {
+@Table(name = "tb_resource")
+public class Resource {
     @EqualsAndHashCode.Include
     @NonNull
     @Id
@@ -32,19 +29,25 @@ public class Offer {
     private Long id;
 
     @NonNull
-    private String edition;
+    private String title;
 
     @NonNull
-    private Instant startMoment;
+    private String description;
 
     @NonNull
-    private Instant endMoment;
+    private Integer position;
+
+    @NonNull
+    private String imgUri;
+
+    @NonNull
+    private ResourceType type;
+
+    @NonNull
+    private String externalLink;
 
     @NonNull
     @ManyToOne
-    @JoinColumn(name = "course_id")
-    private Course course;
-
-    @OneToMany(mappedBy = "offer")
-    private final List<Resource> resources = new ArrayList<>();
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
 }
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,19 +1,23 @@
-INSERT INTO tb_role(authority) VALUES ('ROLE_STUDENT');
-INSERT INTO tb_role(authority) VALUES ('ROLE_INSTRUCTOR');
-INSERT INTO tb_role(authority) VALUES ('ROLE_ADMIN');
+INSERT INTO tb_role (authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role (authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+ 
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-INSERT INTO tb_user(name, email, password) VALUES ('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO tb_user(name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
-INSERT INTO tb_user(name, email, password) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_user_role(user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (2, 1);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (2, 2);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (3, 1);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (3, 2);
-INSERT INTO tb_user_role(user_id, role_id) VALUES (3, 3);
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://cursos-gratuitos-sp.com/wp-content/uploads/2020/03/cursos-gratuitos-duque-de-caxias.jpg', 'https://coerveracademy.es/wp-content/uploads/2016/11/Foto1.png');
 
-INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://cursos-gratuitos-sp.com/wp-content/uploads/2020/03/cursos-gratuitos-duque-de-caxias.jpg', 'https://coerveracademy.es/wp-content/uploads/2016/11/Foto1.png');
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
 
-INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
-INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Trilha HTML', 'Trilha Principal do Curso', 1, 'https://cursos-gratuitos-sp.com/wp-content/uploads/2020/03/cursos-gratuitos-duque-de-caxias.jpg', 1, '', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Fórum', 'Tire suas dúvidas', 2, 'https://cursos-gratuitos-sp.com/wp-content/uploads/2020/03/cursos-gratuitos-duque-de-caxias.jpg', 2, '', 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, external_link, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://cursos-gratuitos-sp.com/wp-content/uploads/2020/03/cursos-gratuitos-duque-de-caxias.jpg', 0, '', 1);
