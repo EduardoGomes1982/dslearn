5701d5089db0cd6a2439aae798f2a9630d03ee17
EduardoGomes1982 <edu1982talk@gmail.com>
2023-07-18 21:51:29 -0300

    Start classes, seed +, mapped classes

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Content.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_content")
+public class Content extends Lesson {
+    private String textContent;
+    private String videoUri;
+
+    public Content(Long id, String title, Integer position, Section section, String textContent, String videoUri) {
+        super(id, title, position, section);
+        this.textContent = textContent;
+        this.videoUri = videoUri;
+    }
+}
---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -0,0 +1,53 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "tb_lesson")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Lesson {
+    @EqualsAndHashCode.Include
+    @NonNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    @NonNull
+    private Integer position;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "section_id")
+    private Section section;
+
+    @ManyToMany
+    @JoinTable(name = "tb_lessons_done", joinColumns = @JoinColumn(name = "lesson_id"), inverseJoinColumns = {
+            @JoinColumn(name = "user_id"),
+            @JoinColumn(name = "offer_id") })
+    private final Set<Enrollment> enrollmentsDone = new HashSet<>();
+}
