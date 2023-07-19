7c037023a459f857f10a9d062734ecb5a9ec81e0
EduardoGomes1982 <edu1982talk@gmail.com>
2023-07-18 23:35:57 -0300

    Imports deteled

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Notification.java
@@ -11,8 +11,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import lombok.AccessLevel;
 import lombok.Data;
