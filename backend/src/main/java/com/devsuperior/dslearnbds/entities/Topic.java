f82fc084fb73ec371249156c399a7c50498d8662
EduardoGomes1982 <edu1982talk@gmail.com>
2023-07-19 21:27:49 -0300

    Topic start, mapped, seed

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Topic.java
@@ -0,0 +1,70 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "tb_topic")
+public class Topic {
+    @EqualsAndHashCode.Include
+    @NonNull
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private String title;
+
+    @NonNull
+    @Column(columnDefinition = "text")
+    private String body;
+
+    @Setter(AccessLevel.NONE)
+    @Column(columnDefinition = "timestamp with time zone")
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "offer_id")
+    private Offer offer;
+
+    @ManyToOne
+    @JoinColumn(name = "lesson_id")
+    private Lesson lesson;
+
+    @ManyToMany
+    @JoinTable(name = "tb_topic_likes", joinColumns = @JoinColumn(name = "topic_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> likes = new HashSet<>();
+
+    @PrePersist
+    private void prePersist() {
+        moment = Instant.now();
+    }
+}
