3a6f6c2dfca13960912801d0a32806a77ed09e27
EduardoGomes1982 <edu1982talk@gmail.com>
2023-09-17 08:22:45 -0300

    Preauthorizing methods

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/DeliverRevisionDTO.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dslearnbds.dto;
+
+import com.devsuperior.dslearnbds.entities.enums.DeliverStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeliverRevisionDTO {
+    private DeliverStatus status;
+    private String feedback;
+    private Integer correctCount;
+}
